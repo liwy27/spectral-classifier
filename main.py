d8a61e85108ea1150cf069281507e66c9ae0ca3b
liwy27 <2316834847@qq.com>
2020-10-09 20:51:41 +0800

    add main file

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 import tensorflow as tf
 import logging
 import sys
-import pandas as pd
 
 
 NUMERIC_COLUMNS = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth']
@@ -14,8 +13,8 @@ FLAGS = tf.app.flags.FLAGS
 tf.app.flags.DEFINE_string("train_paths", './data/iris_training.csv', "The train data ")
 tf.app.flags.DEFINE_string("eval_paths", './data/iris_test.csv', "The eval data")
 tf.app.flags.DEFINE_string("model_path", './model', "The output directory where the model checkpoints will be written.")
-tf.app.flags.DEFINE_integer('batch_size', 6, 'Instance count in a batch.')
-tf.app.flags.DEFINE_integer('train_num_epochs', 100, 'Iteration count over training data.')
+tf.app.flags.DEFINE_integer('batch_size', 4, 'Instance count in a batch.')
+tf.app.flags.DEFINE_integer('train_num_epochs', 20, 'Iteration count over training data.')
 tf.app.flags.DEFINE_integer("eval_num_epochs", 1, "Total batch size for eval.")
 tf.app.flags.DEFINE_float("learning_rate", 0.0005, "The initial learning rate for Adam.")
 
@@ -35,7 +34,7 @@ def input_fn_builder(file_pattern, num_epochs):
             batch_size=FLAGS.batch_size,
             num_epochs=num_epochs,
             num_rows_for_inference=1,
-            shuffle=True)
+            shuffle=False)
     return input_fn
 
 
@@ -48,30 +47,26 @@ def serving_input_receiver_fn():
 
 
 def main(_):
-    run_config = tf.estimator.RunConfig(keep_checkpoint_max=1, save_checkpoints_steps=10, log_step_count_steps=1)
+
+    run_config = tf.estimator.RunConfig(keep_checkpoint_max=1, save_checkpoints_steps=10, log_step_count_steps=10)
 
     classifier_estimator = tf.estimator.DNNClassifier(
         hidden_units=[10, 10],
         feature_columns=feature_columns,
         model_dir=FLAGS.model_path,
         n_classes=3,
-        optimizer='Adam',
-        dropout=0.1,
+        optimizer='Adagrad',
         config=run_config
     )
 
     train_input_fn = input_fn_builder(FLAGS.train_paths, FLAGS.train_num_epochs)
     eval_input_fn = input_fn_builder(FLAGS.eval_paths, FLAGS.eval_num_epochs)
 
-    # tf.estimator.train_and_evaluate(
-    #     estimator=classifier_estimator,
-    #     train_spec=tf.estimator.TrainSpec(input_fn=train_input_fn),
-    #     eval_spec=tf.estimator.EvalSpec(input_fn=eval_input_fn)
-    # )
-
-    classifier_estimator.train(
-        input_fn=train_input_fn,
-        steps=5)
+    tf.estimator.train_and_evaluate(
+        estimator=classifier_estimator,
+        train_spec=tf.estimator.TrainSpec(input_fn=train_input_fn),
+        eval_spec=tf.estimator.EvalSpec(input_fn=eval_input_fn, steps=10)
+    )
 
     if classifier_estimator.config.is_chief:
         classifier_estimator.export_saved_model(
